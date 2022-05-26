027d6528513c1236fd9070e4bb2b7ee612a02aaf
Prajakta Badade <69361359+hinata1826@users.noreply.github.com>
2022-05-26 21:29:25 +0530

    Update core.module.ts

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -13,7 +13,6 @@ import {MatDividerModule} from '@angular/material/divider';
   ],
   imports: [
     CommonModule,
-    CommonModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
