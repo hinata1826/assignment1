73956ae07a066eda844528b61d6a60d6c8d6e21e
Prajakta Badade <69361359+Prajakta-Badade@users.noreply.github.com>
2022-07-14 17:00:57 +0530

    Update login.component.ts

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -10,9 +10,7 @@ export class LoginComponent implements OnInit {
   
   
   
-    /**
-     * Database driver used by connection.
-     */
+   
     driver: CockroachDriver;
 
     // -------------------------------------------------------------------------
