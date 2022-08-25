5c3f3a66bf4536faf7c95ba8414eab4133a23f69
Prajakta-Badade <69361359+Prajakta-Badade@users.noreply.github.com>
2022-08-25 11:41:15 +0530

    Update login.component.ts

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -40,7 +40,7 @@ export class LoginComponent implements OnInit {
     // -------------------------------------------------------------------------
     // Constructor
     // -------------------------------------------------------------------------
-
+    unuse
     constructor(driver: CockroachDriver, mode: ReplicationMode) {
         super();
         this.driver = driver;
